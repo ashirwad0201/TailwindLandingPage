726a7e06fe0ce96147791bcb9e0259e65f6cfec8
ashirwad0201 <ashirwad0201@gmail.com>
2024-05-05 12:46:29 +0530

    image of the body

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Body = () => {
   return (
-    <div>Body</div>
-  )
-}
+    <div>
+      <div className="flex items-center justify-center">
+        <img src="./assets/Blue-Shape.svg" alt="1st" className="-rotate-45 h-64"/>
+        <img src="./assets/Pink-Shape.svg" alt="2nd" className="absolute -rotate-[30deg] h-64"/>
+        <img src="./assets/Purple-Shape.svg" alt="3rd" className="absolute -rotate-[15deg] h-64"/>
+        <img src="./assets/Hero-Model.png" alt="hero" className="absolute h-64"/>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
